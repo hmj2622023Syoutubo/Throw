4c85164a2aea510fbf3908500e8c2909f29a6b53
hmj2622023Syoutubo <hmj2622023@stu.o-hara.ac.jp>
2026-06-30 15:01:44 +0900

    少し変更

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,14 +4,6 @@ using UnityEngine.SceneManagement;
 public class Manager : MonoBehaviour
 {
 	[SerializeField] GameObject generator;
-	[SerializeField] AudioClip BGM;
-	AudioSource aud;
-
-	private void Start()
-	{
-		aud = GetComponent<AudioSource>();
-		aud.PlayOneShot(BGM);
-	}
 	private void Update()
 	{
 		GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
